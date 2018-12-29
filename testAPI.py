4ef0256ba0470bb9b0823fafeadf7210accc3908
Idan <idan.hovav16@gmail.com>
2018-12-28 19:03:31 -0800

    Test addPurchase endpoint

--- testAPI.py
+++ testAPI.py
@@ -34,11 +34,9 @@ class TestAPI(TestCase):
     def testCreateAccount(self):
         params = {"name": "Idan", "emailAddress": "idan@hovav.com"}
         createAccountResponse = requests.post(serverURL + "/account/create", data=params)
-        print(createAccountResponse.text)
         self.assertTrue(createAccountResponse.status_code == 200)
         createAccountJson = createAccountResponse.json()
         newAccountID = createAccountJson["accountID"]
-        print("AccountID: %s " % newAccountID)
         self.assertTrue(newAccountID)
 
         getAccountInfoResponse = requests.get(serverURL + "/account/" + newAccountID + "/info")
@@ -63,6 +61,23 @@ class TestAPI(TestCase):
         tripID = createTripResponse.json()["tripID"]
         self.assertTrue(tripID)
 
+    def testAddPurchase(self):
+        createOwnerParams = {"name": "Idan", "emailAddress": "idan@hovav.com", "password": "abcdef"}
+        createAccountParams = [{"name": "Bob", "emailAddress": "Bob@billy.com"}, {"name": "Jim", "emailAddress": "Jim@john.com"}]
+        createOwnerResponse = requests.post(serverURL + "/account/create", data=createOwnerParams)
+        createAccountResponses = [requests.post(serverURL + "/account/create", data=createAccountParam) for createAccountParam in createAccountParams]
+        ownerAccountID = createOwnerResponse.json()["accountID"]
+        accountIDs = [createAccountResponse.json()["accountID"] for createAccountResponse in createAccountResponses]
+        tripMemberAccountIDs = ",".join([ownerAccountID] + accountIDs)
+        createTripParams = {"creatorAccountID": ownerAccountID, "creatorAccountPassword": "abcdef", "tripName": "test", "tripMemberAccountIDs": tripMemberAccountIDs}
+        createTripResponse = requests.post(serverURL + "/trip/create", data=createTripParams)
+        tripID = createTripResponse.json()["tripID"]
+
+        addPurchaseParams = {"purchaserAccountID": accountIDs[0], "purchaseAmount": 23, "description": "gas"}
+        addPurchaseResponse = requests.post(serverURL + "/trip/" + tripID + "/addPurchase", data=addPurchaseParams)
+        self.assertTrue(addPurchaseResponse.status_code == 200)
+        purchaseID = addPurchaseResponse.json()["purchaseID"]
+        self.assertTrue(purchaseID)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
