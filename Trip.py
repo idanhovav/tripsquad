695e9b697db5e5b52f4292c0e712ac4155da6682
Idan <idan.hovav16@gmail.com>
2018-12-30 17:42:52 -0800

    Remove initializing a trip with purchases

--- Trip.py
+++ Trip.py
@@ -5,12 +5,11 @@ import TripMember
 
 class Trip:
 
-    def __init__(self, accountIDs, purchaseIDs = [], tripName="", tripDescription=""):
+    def __init__(self, accountIDs, tripName="", tripDescription=""):
         self.ID = str(uuid.uuid4())
         self.timeStamp = str(dt.datetime.today())
         tripMembers =  set([TripMember.TripMember(accountID, self.ID) for accountID in accountIDs])
         self.tripMemberIDs = [tripMember.ID for tripMember in tripMembers]
-        self.purchaseIDs = list(set(purchaseIDs)) # no repeats
         self.tripName = tripName
         self.tripDescription = tripDescription
 
