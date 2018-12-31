5bda6bc8716bd8f00d316bad32cf404bb39a7897
Idan <idan.hovav16@gmail.com>
2018-12-30 16:57:36 -0800

    Make TripMemberID concat of Acct and Trip IDs

--- TripMember.py
+++ TripMember.py
@@ -1,10 +1,20 @@
+import datetime as dt
 import db
+import utils
 
 class TripMember:
-    def __init__(self, accountID, tripID):
-        self.TripMemberID = str(uuid.uuid4())
+    def __init__(self, AccountID, TripID):
+        self.TripMemberID = getTripMemberID(AccountID, TripID)
         self.timeStamp = str(dt.datetime.today())
-        self.accountID = accountID
-        self.tripID = tripID
+        self.AccountID = AccountID
+        self.TripID = TripID
         self.totalPurchaseAmount = 0
-        db.tripMemberByID[self.TripMemberID] = self
+        db.tripMembersByID[self.TripMemberID] = self
+
+def getTripAndAccountIDs(TripMemberID):
+
+    return TripMemberID.split(utils.IDCharSeparator)
+
+def getTripMemberID(AccountID, TripID):
+
+    return AccountID + utils.IDCharSeparator + TripID
--- utils.py
+++ utils.py
@@ -4,3 +4,5 @@ def hasExpectedParams(expectedParams, request):
             return False
 
     return True
+
+IDCharSeparator = "~"
