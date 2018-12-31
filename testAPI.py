49980c6f944e5b05bbe7f74398f81e9395e7cb15
Idan <idan.hovav16@gmail.com>
2018-12-30 20:45:27 -0800

    Modularize test functionality

--- testAPI.py
+++ testAPI.py
@@ -11,6 +11,12 @@ sleepTimer = 2 # seconds
 
 serverURL = "http://0.0.0.0:5000"
 
+testAccounts = [
+    {"name": "Dan", "emailAddress": "dan@dan.com", "password": "abcdef"},
+    {"name": "Bob", "emailAddress": "bob@bob.com", "password": "123456"},
+    {"name": "Jim", "emailAddress": "jim@jim.com", "password": "nopqrs"}
+]
+
 class TestAPI(TestCase):
     serverProcess = None
 
@@ -30,79 +36,87 @@ class TestAPI(TestCase):
             print("Terminating API server")
             TestAPI.serverProcess.terminate()
 
-
     def testCreateAccount(self):
-        createAccountParams = {"name": "Idan", "emailAddress": "idan@hovav.com", "password": "123456"}
-        createAccountResponse = requests.post(serverURL + "/account/create", data=createAccountParams)
-        self.assertTrue(createAccountResponse.status_code == 200)
-        createAccountJson = createAccountResponse.json()
-        newAccountID = createAccountJson["accountID"]
-        self.assertTrue(newAccountID)
-
-        getAccountInfoParams = {"password": "123456"}
-        getAccountInfoResponse = requests.post(serverURL + "/account/" + newAccountID + "/info", data=getAccountInfoParams)
-        self.assertTrue(getAccountInfoResponse.status_code == 200)
-        getAccountInfoJson = getAccountInfoResponse.json()
-        expectedParams = ["name", "emailAddress", "accountID"]
-        self.assertTrue(all([param in getAccountInfoJson for param in expectedParams]))
-        self.assertTrue(getAccountInfoJson["name"] == createAccountParams["name"])
-        self.assertTrue(getAccountInfoJson["emailAddress"] == createAccountParams["emailAddress"])
+        for i in range(len(testAccounts)):
+            testAccount = testAccounts[i]
+            accountInfo = self.createAccount(testAccount)
+            self.assertTrue(accountInfo["name"] == testAccount["name"])
+            self.assertTrue(accountInfo["emailAddress"] == testAccount["emailAddress"])
+            self.assertTrue(accountInfo["accountID"])
 
     def testCreateTrip(self):
-        createOwnerParams = {"name": "Idan", "emailAddress": "idan@hovav.com", "password": "abcdef"}
-        createAccountParams = {"name": "Bob", "emailAddress": "Bob@billy.com", "password": "ghiklm"}
-        createOwnerResponse = requests.post(serverURL + "/account/create", data=createOwnerParams)
-        createAccountResponse = requests.post(serverURL + "/account/create", data=createAccountParams)
-        ownerAccountID = createOwnerResponse.json()["accountID"]
-        accountID = createAccountResponse.json()["accountID"]
-
-        tripMemberAccountIDs = ",".join([ownerAccountID, accountID])
-        createTripParams = {"creatorAccountID": ownerAccountID, "creatorAccountPassword": "abcdef", "tripName": "test", "tripMemberAccountIDs": tripMemberAccountIDs}
-        createTripResponse = requests.post(serverURL + "/trip/create", data=createTripParams)
-        self.assertTrue(createTripResponse.status_code == 200)
-        tripID = createTripResponse.json()["tripID"]
+        accountIDs = [self.createAccount(account)["accountID"] for account in testAccounts[0:2]]
+        password = testAccounts[0]["password"]
+        tripID = self.createTrip(accountIDs, password, "testCreateTrip")
         self.assertTrue(tripID)
 
     def testAddPurchase(self):
-        createOwnerParams = {"name": "Idan", "emailAddress": "idan@hovav.com", "password": "abcdef"}
-        createAccountParams = [{"name": "Bob", "emailAddress": "Bob@billy.com", "password": "ghiklm"}, {"name": "Jim", "emailAddress": "Jim@john.com", "password": "nopqrs"}]
-        createOwnerResponse = requests.post(serverURL + "/account/create", data=createOwnerParams)
-        createAccountResponses = [requests.post(serverURL + "/account/create", data=createAccountParam) for createAccountParam in createAccountParams]
-        ownerAccountID = createOwnerResponse.json()["accountID"]
-        accountIDs = [createAccountResponse.json()["accountID"] for createAccountResponse in createAccountResponses]
-        tripMemberAccountIDs = ",".join([ownerAccountID] + accountIDs)
-        createTripParams = {"creatorAccountID": ownerAccountID, "creatorAccountPassword": "abcdef", "tripName": "test", "tripMemberAccountIDs": tripMemberAccountIDs}
-        createTripResponse = requests.post(serverURL + "/trip/create", data=createTripParams)
-        tripID = createTripResponse.json()["tripID"]
+        accountIDs = [self.createAccount(account)["accountID"] for account in testAccounts]
+        tripCreatorPassword = testAccounts[0]["password"]
+        tripID = self.createTrip(accountIDs, tripCreatorPassword, "testAddPurchase")
 
-        addPurchaseParams = {"purchaserAccountID": accountIDs[0], "purchaserAccountPassword": "ghiklm", "purchaseAmount": 23, "description": "gas"}
-        addPurchaseResponse = requests.post(serverURL + "/trip/" + tripID + "/addPurchase", data=addPurchaseParams)
-        self.assertTrue(addPurchaseResponse.status_code == 200)
-        purchaseID = addPurchaseResponse.json()["purchaseID"]
-        self.assertTrue(purchaseID)
+        for i in range(len(testAccounts)):
+            testAccount = testAccounts[i]
+            password = testAccount["password"]
+            purchaseID = self.addPurchase(accountIDs[i], tripID, password, 10**i, "purchase" + str(i))
+            self.assertTrue(purchaseID)
 
     def testGetTripTotal(self):
-        createOwnerParams = {"name": "Idan", "emailAddress": "idan@hovav.com", "password": "abcdef"}
-        createAccountParams = [{"name": "Bob", "emailAddress": "Bob@billy.com", "password": "ghiklm"}, {"name": "Jim", "emailAddress": "Jim@john.com", "password": "nopqrs"}]
-        createOwnerResponse = requests.post(serverURL + "/account/create", data=createOwnerParams)
-        createAccountResponses = [requests.post(serverURL + "/account/create", data=createAccountParam) for createAccountParam in createAccountParams]
-        ownerAccountID = createOwnerResponse.json()["accountID"]
-        accountIDs = [createAccountResponse.json()["accountID"] for createAccountResponse in createAccountResponses]
-        tripMemberAccountIDs = ",".join([ownerAccountID] + accountIDs)
-        createTripParams = {"creatorAccountID": ownerAccountID, "creatorAccountPassword": "abcdef", "tripName": "test", "tripMemberAccountIDs": tripMemberAccountIDs}
-        createTripResponse = requests.post(serverURL + "/trip/create", data=createTripParams)
-        tripID = createTripResponse.json()["tripID"]
+        accountIDs = [self.createAccount(account)["accountID"] for account in testAccounts]
 
-        addPurchaseParams = [{"purchaserAccountID": accountIDs[0], "purchaserAccountPassword": "ghiklm", "purchaseAmount": 23, "description": "gas"}, {"purchaserAccountID": accountIDs[1], "purchaserAccountPassword": "nopqrs", "purchaseAmount": 100, "description": "lodging"}]
+        tripCreatorPassword = testAccounts[0]["password"]
+        tripID = self.createTrip(accountIDs, tripCreatorPassword, "testAddPurchase")
 
-        addPurchaseResponses = [requests.post(serverURL + "/trip/" + tripID + "/addPurchase", data=addPurchaseParam) for addPurchaseParam in addPurchaseParams]
-        purchaseIDs = [addPurchaseResponse.json()["purchaseID"] for addPurchaseResponse in addPurchaseResponses]
+        for i in range(len(testAccounts)):
+            testAccount = testAccounts[i]
+            password = testAccount["password"]
+            purchaseID = self.addPurchase(accountIDs[i], tripID, password, 10**i, "purchase" + str(i))
+            self.assertTrue(purchaseID)
 
-        getTotalParams = {"accountID": ownerAccountID, "password": "abcdef"}
-        getTotalResponse = requests.post(serverURL + "/trip/" + tripID + "/getTotal", data=getTotalParams)
-        self.assertTrue(getTotalResponse.status_code == 200)
-        tripTotal = getTotalResponse.json()["total"]
-        self.assertTrue(tripTotal == 123)
+        getTripTotalParams = {"accountID": accountIDs[0], "password": testAccounts[0]["password"]}
+        getTripTotalJson = self.sendRequest("/trip/" + tripID + "/getTotal", getTripTotalParams, ["total"])
+        tripTotal = getTripTotalJson["total"]
+        self.assertTrue(tripTotal == 111)
+
+    def createAccount(self, params):
+        createAccountJson = self.sendRequest("/account/create", params, ["accountID"])
+        newAccountID = createAccountJson["accountID"]
+
+        getAccountInfoJson = self.sendRequest("/account/" + newAccountID + "/info", params, ["name", "emailAddress", "accountID"])
+        return getAccountInfoJson
+
+    def createTrip(self, accountIDs, password, tripName):
+        tripMemberAccountIDs = ",".join(accountIDs)
+        params = {"creatorAccountID": accountIDs[0], 
+            "creatorAccountPassword": password, 
+            "tripName": tripName, 
+            "tripMemberAccountIDs": tripMemberAccountIDs}
+        createTripJson = self.sendRequest("/trip/create", params, ["tripID"])
+        tripID = createTripJson["tripID"]
+        self.assertTrue(tripID)
+        return tripID
+
+    def addPurchase(self, accountID, tripID, password, amount, description):
+        params = {"purchaserAccountID": accountID,
+            "purchaserAccountPassword": password, 
+            "purchaseAmount": amount, 
+            "description": description}
+
+        addPurchaseJson = self.sendRequest("/trip/" + tripID + "/addPurchase", params, ["purchaseID"])
+        purchaseID = addPurchaseJson["purchaseID"]
+        self.assertTrue(purchaseID)
+        return purchaseID
+
+    def checkResponseJson(self, responseJson, expectedResponseKeys):
+
+        self.assertTrue(all([key in responseJson for key in expectedResponseKeys]))
+
+    def sendRequest(self, path, data, expectedResponseKeys):
+        receivedResponse = requests.post(serverURL + path, data=data)
+        self.assertTrue(receivedResponse.status_code == 200)
+        responseJson = receivedResponse.json()
+        self.checkResponseJson(responseJson, expectedResponseKeys)
+        return responseJson
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
